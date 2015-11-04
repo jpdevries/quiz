fe1ec7a0161eac04228af7acefb88c027c028696
Nathan McElwain <nathanmcelwain@ymail.com>
2015-11-04 11:11:18 -0800

    some styling and JS

--- js/app.js
+++ js/app.js
@@ -1,11 +1,39 @@
 $(document).ready(function(){
-	$('#ques').hide();
-	$('#ans').hide();
+	
+
+	var ques = {
+		one:"Who was Dante's muse?",
+		two:"What is the name of Dante's treatsie on the vernacular called?",
+		three:"From what Italian city did Dante hail from?",
+		four:"How many circles did Dante determine the Inferno to have?",
+		five:"Who did Dante put in the Beast's central mouth?"
+	};
+
+	$('#card').hide();
+	
 
 	$('#button').on('click', function(){
-		$('#intro').slideUp(500);
-		$('#ques').show();
+		$('#intro').hide();
+		$('#card').fadeIn();
+		
+	
 	});
 
+	$('#card').on('click',function(){
+		//console.log("clicked");
+		$('#card').flip({
+			front:'auto',
+			axis:'x',
+			speed:'100'
+		});
+		$('.back').show();
+
+	});
+
+	$('#ques').text(ques.four);
+	$('#ans').text("Beatrice");
+
+	//var answers = [1:4,2:2,3:1,4:2,5:3];
 	
+
 });
